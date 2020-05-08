7c0faca6248ff047f209b44b3233f7060bbc83b9
Elan <elan.kvitko@gmail.com>
2020-05-08 10:42:55 -0400

    Add route for creating like

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :dogs
+
+  resources :dogs do
+    # Here I am only adding a create route as per the requirement to
+    # allow a user to like (CREATE)
+    resources :likes, only: [:create]
+  end
+
   root to: "dogs#index"
 end
